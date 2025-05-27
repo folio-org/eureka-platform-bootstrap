8ec1f908ce09fa1317c0737b45480865704ae9c8
Yauhen Vavilkin <yauhen_vavilkin@epam.com>
2025-05-27 12:24:00 +0400

    fix doubled vault tokens (#20)

--- docker/misc/get-vault-token.sh
+++ docker/misc/get-vault-token.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")" || exit
 
 docker logs vault 2> /dev/null \
   | grep "Root VAULT TOKEN is:" \
-  | sed -E 's/^.*Root VAULT TOKEN is: (.+)$/\1/'
+  | sed -E 's/^.*Root VAULT TOKEN is: (.+)$/\1/' \
+  | tail -n 1
